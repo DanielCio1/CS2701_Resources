d15c0b87671b6225ed601f387b8542978c8d278e
Iulian Daniel Ciolan <2335984@brunel.ac.uk>
2026-03-10 14:38:56 +0000

    Add files via upload

---
+++ Labs/Week 20 Lab/Backend/demo/src/test/java/groupa/_2/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package groupa._2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
